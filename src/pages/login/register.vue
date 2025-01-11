074483ada45f7cc814c54c092fb01347c3fbb490
张浩 <oncwnuPJ8pf4MmE_-MlN-l5iVOIk@git.weixin.qq.com>
2025-01-11 14:57:45 +0800

    misc: change apis starting from register

--- src/pages/login/register.vue
+++ src/pages/login/register.vue
@@ -9,13 +9,16 @@
       <view class="box pt-50">
         <tm-form @submit="confirm" ref="form" v-model="loginForm" :label-width="190">
           <tm-form-item required field="username" :rules="[{ required: true, message: '请输入账号' }]">
-            <tm-input :inputPadding="[20, 0]" :round="20" prefix="tmicon-account" v-model.lazy="loginForm.username" placeholder="请输入账号" :showBottomBotder="false"> </tm-input>
+            <tm-input :inputPadding="[20, 0]" :round="20" prefix="tmicon-account" v-model.lazy="loginForm.username"
+              placeholder="请输入账号" :showBottomBotder="false"> </tm-input>
           </tm-form-item>
           <tm-form-item required field="password" :rules="[{ required: true, message: '请输入密码' }]">
-            <tm-input :inputPadding="[20, 0]" type="password" :round="20" prefix="tmicon-lock" v-model.lazy="loginForm.password" placeholder="请输入密码" :showBottomBotder="false"> </tm-input>
+            <tm-input :inputPadding="[20, 0]" type="password" :round="20" prefix="tmicon-lock"
+              v-model.lazy="loginForm.password" placeholder="请输入密码" :showBottomBotder="false"> </tm-input>
           </tm-form-item>
           <tm-form-item :border="false">
-            <tm-button :margin="[10]" :shadow="0" :round="20" size="small" form-type="submit" block label="立即注册"></tm-button>
+            <tm-button :margin="[10]" :shadow="0" :round="20" size="small" form-type="submit" block
+              label="立即注册"></tm-button>
             <view class="login-tips flex flex-between px-30">
               <text class="" @click="openLink('pages/login/forgot')">忘记密码</text>
               <text class="" @click="openLink('pages/login/login', 1)">已有账号?立即登录</text>
@@ -30,32 +33,33 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { openLink } from '@/common/tools';
-import { register, getCaptcha } from '@/common/index';
+import { register } from '@/common/index';
 import { useAppStore } from '@/stores/app';
 const appStore = useAppStore();
-const captchaUrl = ref('');
 const loginForm = ref({
   username: '',
   password: '',
-  captcha: '',
 });
-function captcha() {
-  getCaptcha({ scene: 'register' }).then(res => {
-    if (res.code == 1000) {
-      captchaUrl.value = res.data;
-    }
-  });
-}
-captcha();
+
 function confirm(e: any) {
   if (e.validate) {
+    uni.request({
+      url: "/ballkeeper/",
+      method: 'GET',
+      success: (res) => {
+        console.log("request ok, res: ", res);
+      },
+      fail: (err) => {
+        console.error('request failed: ', err);
+      }
+    });
+
     register({
       ...e.data,
     }).then(res => {
       if (res.code == 1000) {
         openLink('pages/login/login', 1);
-      }else{
-        captcha();
+      } else {
         uni.$tm.u.toast(res.message);
       }
     });
@@ -68,6 +72,7 @@ function confirm(e: any) {
   background-color: #3c8af8;
   height: 350rpx;
   position: relative;
+
   .logo {
     position: absolute;
     left: 50%;
@@ -76,6 +81,7 @@ function confirm(e: any) {
     z-index: 100;
   }
 }
+
 .login {
   flex: 1;
   margin-top: -170rpx;
@@ -84,16 +90,17 @@ function confirm(e: any) {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
   .box {
     height: 100%;
     background-color: #fff;
     border-top-right-radius: 15rpx;
     border-top-left-radius: 15rpx;
+
     .login-tips {
       color: #3c8af8;
       font-size: 24rpx;
       margin-top: 30rpx;
     }
   }
-}
-</style>
+}</style>
