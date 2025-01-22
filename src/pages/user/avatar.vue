d572a38ff1e6099e62992bad149c606e0ebd1bd3
张浩 <oncwnuPJ8pf4MmE_-MlN-l5iVOIk@git.weixin.qq.com>
2025-01-22 09:21:39 +0800

    add: button in avatar page

--- src/pages/user/avatar.vue
+++ src/pages/user/avatar.vue
@@ -8,13 +8,21 @@
       </view>
     </view>
     <view style="margin-top: 100px; padding: 0 40px;">
-        <tm-button :margin="[10]" :shadow="0" :round="20" size="small" form-type="submit" block label="完成"></tm-button>
+        <tm-button 
+          :margin="[10]" 
+          :shadow="0" 
+          :round="20" 
+          size="small" 
+          block 
+          :label="hasUploadedImage ? '完成' : '跳过头像上传'" 
+          @click="handleButtonClick"
+        ></tm-button>
     </view>
   </tm-app>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import {useUserStore} from '@/stores/user';
 import {editUserInfo} from '@/common/index';
 import { openLink, debugLog } from '@/common/tools';
@@ -31,10 +39,40 @@ const editForm = ref({
   password: '',
 });
 
+const hasUploadedImage = ref(false);  // 用于跟踪是否有上传的图片
+
 onLoad((e: any) => {
   console.log("[onLoad] e: ", e);
   username.value = e.username;
 });
+
+// 监听图片上传状态
+watch(() => editForm.value.avatar, (newVal) => {
+  hasUploadedImage.value = !!newVal;
+});
+
+// 按钮点击处理
+const handleButtonClick = () => {
+  if (hasUploadedImage.value) {
+    // 有图片时直接跳转
+    uni.navigateTo({
+      url: '/pages/login/login'
+    });
+  } else {
+    // 无图片时显示确认提示
+    uni.showModal({
+      title: '提示',
+      content: '确认跳过头像上传吗？',
+      success: (res) => {
+        if (res.confirm) {
+          uni.navigateTo({
+            url: '/pages/login/login'
+          });
+        }
+      }
+    });
+  }
+};
 
 function confirm(e:any) {
 
