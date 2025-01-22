7caa0ea709c997fbcc883ab1b086cfaceff3a895
张浩 <oncwnuPJ8pf4MmE_-MlN-l5iVOIk@git.weixin.qq.com>
2025-01-22 13:48:32 +0800

    misc: adjust home page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,27 +4,16 @@
       <dx-home v-if="act_ind === 0"></dx-home>
       <dx-my-activity :category-list="categoryList" v-if="act_ind === 0"></dx-my-activity>
       <dx-activity :category-list="categoryList" v-else-if="act_ind === 1"></dx-activity>
-      <dx-team :category-list="categoryList" v-else-if="act_ind == 3"></dx-team>
-      <dx-member v-else-if="4"></dx-member>
+      <dx-team :category-list="categoryList" v-else-if="act_ind === 2"></dx-team>
+      <dx-member v-else-if="act_ind === 3"></dx-member>
     </tm-sheet>
     <view style="height: 150rpx"></view>
     <tm-tabbar :autoSelect="false" v-model:active="act_ind" :showSafe="false" @change="tabbarChange">
       <tm-tabbar-item @click="act_ind = 0" activeColor="#3c8af8" open-type="reLaunch" text="活动" icon="tmicon-md-home"></tm-tabbar-item>
       <tm-tabbar-item @click="act_ind = 1" activeColor="#3c8af8" text="球队" icon="tmicon-flag-fill"></tm-tabbar-item>
-      <tm-tabbar-item
-        :shadow="2"
-        btn-top
-        fontColor="white"
-        activeColor="white"
-        linear="top"
-        linearDeep="accent"
-        color="#38cdde"
-        icon="tmicon-paperplane-fill"
-        url="/others/publish/publish"
-        text="发起" 
-      ></tm-tabbar-item>
-      <tm-tabbar-item @click="act_ind = 3" activeColor="#3c8af8" text="待定" icon="tmicon-layergroup-fill"></tm-tabbar-item>
-      <tm-tabbar-item @click="act_ind = 4" activeColor="#3c8af8" text="我的" icon="tmicon-md-person"></tm-tabbar-item>
+      <!-- <tm-tabbar-item :shadow="2" btn-top fontColor="white" activeColor="white" linear="top" linearDeep="accent" color="#38cdde" icon="tmicon-paperplane-fill" url="/others/publish/publish" text="发起" ></tm-tabbar-item> -->
+      <tm-tabbar-item @click="act_ind = 2" activeColor="#3c8af8" text="待定" icon="tmicon-layergroup-fill"></tm-tabbar-item>
+      <tm-tabbar-item @click="act_ind = 3" activeColor="#3c8af8" text="我的" icon="tmicon-md-person"></tm-tabbar-item>
     </tm-tabbar>
   </tm-app>
 </template>
@@ -53,15 +42,15 @@ function tabbarChange(e: number) {
       break;
     case 1:
       uni.setNavigationBarTitle({
-        title: '找活动',
+        title: '球队',
+      });
+      break;
+    case 2:
+      uni.setNavigationBarTitle({
+        title: '待定',
       });
       break;
     case 3:
-      uni.setNavigationBarTitle({
-        title: '找球队',
-      });
-      break;
-    case 4:
       uni.setNavigationBarTitle({
         title: '我的',
       });
