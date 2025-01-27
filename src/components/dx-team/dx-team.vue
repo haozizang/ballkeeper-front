a8d9363e5196ef1c6a32ca8fa4f7876dcec7858c
张浩 <oncwnuPJ8pf4MmE_-MlN-l5iVOIk@git.weixin.qq.com>
2025-01-27 11:06:16 +0800

    misc: fake some team data

--- src/components/dx-team/dx-team.vue
+++ src/components/dx-team/dx-team.vue
@@ -10,72 +10,118 @@
         </tm-tabs>
       </view>
     </view>
-    <view class="list px-30 flex flex-between flex-wrap round-3" :class="{'pb-30':list.length}">
-      <view class="item pb-20 mt-30 round-3" v-for="(item,index) in list" :key="index" @click="openLink('teams/detail/detail?id='+item._id)">
-        <view class="flex-row-center-center pt-30">
-          <tm-avatar :font-size="120" :round="25" :img="item.logo"></tm-avatar>
-        </view>
-        <view class="px-20 mt-50">
-          <view class="title text-overflow-1 text-align-center">{{item.title}}</view>
-          <view class="text-overflow-2 tips my-15">{{ item.content }}</view>
+    <!-- 添加顶部间距，防止被搜索栏遮挡 -->
+    <view class="content-wrapper">
+      <view class="list bg-white">
+        <view class="item flex-row-center-between pa-30" 
+              v-for="(item,index) in teamList" 
+              :key="index" 
+              @click="openLink('teams/detail/detail?id='+item._id)"
+        >
+          <view class="team-content flex flex-row">
+            <!-- 左侧头像 -->
+            <tm-avatar :font-size="120" :round="25" :img="item.logo"></tm-avatar>
+            
+            <!-- 右侧内容 -->
+            <view class="team-info ml-24">
+              <view class="title text-overflow-1">{{item.title}}</view>
+              <view class="tips text-overflow-2 mt-15">{{ item.content }}</view>
+            </view>
+          </view>
         </view>
       </view>
     </view>
   </view>
   <!-- 数据空 -->
-  <view class="load-more px-50 pb-30" v-if="list.length">
+  <view class="load-more px-50 pb-30" v-if="teamList.length">
     <tm-divider align="center" :label="hasMore && loading?'加载中...':hasMore?'上拉加载更多～':'没有更多啦～'"></tm-divider>
   </view>
   <view class="mt-50 py-50" v-else>
-    <tm-result :showBtn="false" title="暂无数据～" subTitle=" " v-if="!list.length && !loading"></tm-result>
+    <tm-result :showBtn="false" title="暂无数据～" subTitle=" " v-if="!teamList.length && !loading"></tm-result>
   </view>
 </template>
 <script lang="ts" setup>
-import { ref, computed,reactive,watch } from 'vue';
-import { teamList } from '@/common/index'
+import { ref, reactive, watch } from 'vue';
 import { openLink } from '@/common/tools';
 import { debounce } from '@/tmui/tool/function/util';
+
 const props = defineProps<{
   categoryList: any[];
 }>();
+
 const hasMore = ref(true);
 const loading = ref(false);
-const list = ref<any[]>([]);
+
+const teamList = ref([
+  { _id: 1, logo: 'https://img1.baidu.com/it/u=3956679599,3956476954&fm=253&fmt=auto&app=138&f=JPEG?w=510&h=500', title: '万众篮球队', content: '内容' },
+  { _id: 2, logo: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png', title: '功夫足球队', content: '足球' },
+  { _id: 3, logo: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png', title: '兄弟连', content: '足球' },
+  { _id: 4, logo: 'https://img1.baidu.com/it/u=1697989098,4244944766&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=586', title: 'FC80S', content: '内容' }
+]);
+
 const param = reactive<any>({
   parent_id: '',
   nextDate: '',
   keyword: '',
-  limit:20
+  limit: 20
 })
-watch(()=>param.keyword, () => {
-  debounce(()=>{
+
+watch(() => param.keyword, () => {
+  debounce(() => {
     getTeamList();
   }, 500)
 })
+
 function tabsChange(e: string) {
   param.parent_id = e;
   getTeamList();
 }
+
 function getTeamList(is_more = false) {
+  loading.value = true;  // 设置加载状态
+  
+  // 模拟 API 调用
+  setTimeout(() => {
+    // 这里暂时使用静态数据，实际项目中应该替换为真实的 API 调用
+    const mockData = teamList.value;
+    
+    if (mockData.length < param.limit) {
+      hasMore.value = false;
+    }
+    
+    if (is_more) {
+      teamList.value = teamList.value.concat(mockData);
+    } else {
+      teamList.value = mockData;
+    }
+    
+    loading.value = false;  // 重置加载状态
+  }, 500);
+  
+  // 当你准备使用真实 API 时，取消注释下面的代码
+  /*
   uni.showLoading({
     title: '加载中...'
   })
   teamList(param).then((res) => {
-    console.log('res', res);
     if (res.code === 1000) {
       if (res.data.length < param.limit || res.data.length === 0) {
         hasMore.value = false
       }
       if (is_more) {
-        list.value = list.value.concat(res.data)
+        teamList.value = teamList.value.concat(res.data)
       } else {
-        list.value = res.data
+        teamList.value = res.data
       }
     }
   }).finally(() => {
     uni.hideLoading()
+    loading.value = false  // 重置加载状态
   });
+  */
 }
+
+// 初始加载
 getTeamList();
 </script>
 <style lang="scss" scoped>
@@ -88,19 +134,62 @@ getTeamList();
   top: 44px;
   // #endif
   left: 0;
+  right: 0;
   z-index: 999;
   background: #ffffff;
 }
-.list {
-  // #ifdef MP-WEIXIN || APP-PLUS
-  padding-top: 190rpx;
+
+// 添加内容区域的样式
+.content-wrapper {
+  // #ifndef H5
+  margin-top: 200rpx;  // 非H5环境
   // #endif
   // #ifdef H5
-  padding-top: calc(130rpx + 30px);
+  margin-top: 244rpx;  // H5环境
   // #endif
+}
+
+.list {
+  width: 100%;
+  
   .item {
-    width: calc((100vw - 90rpx) / 2);
-    box-shadow: 0 0 10rpx rgb(230, 230, 230);
+    border-bottom: 6rpx solid rgb(230, 230, 230);  // 添加底部分割线
+    background-color: #fff;
+    
+    &:last-child {
+      border-bottom: none;  // 最后一个元素不需要分割线
+    }
+  }
+}
+
+.team-content {
+  width: 100%;
+  align-items: flex-start;
+}
+
+.team-info {
+  flex: 1;
+  padding-right: 20rpx;  // 右侧留出一些间距
+}
+
+.title {
+  font-size: 32rpx;
+  font-weight: bold;
+  color: #333;
+}
+
+.tips {
+  font-size: 28rpx;
+  color: #666;
+  line-height: 1.4;
+}
+
+// 添加点击效果
+.item {
+  transition: background-color 0.3s;
+  
+  &:active {
+    background-color: #f5f5f5;
   }
 }
 </style>
