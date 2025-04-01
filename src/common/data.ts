e7b1f2f84512e35f713b5644015bf8f28431e0f9
zhanghao <zhanghao197@foxmail.com>
2025-04-01 16:09:34 +0800

    misc: change id start-value to avoid 0-false mistake

--- src/common/data.ts
+++ src/common/data.ts
@@ -1,22 +1,22 @@
 export const LEAGUE_TYPES = [
-  { id: 0, name: '循环赛' },
-  { id: 1, name: '淘汰赛' }
+  { id: 1, name: '循环赛' },
+  { id: 2, name: '淘汰赛' }
 ];
 
 export const ACT_TYPES = [
-  { id: 0, name: '足球(6人制)' },
-  { id: 1, name: '足球(8人制)' },
-  { id: 2, name: '足球(11人制)' },
-  { id: 3, name: '足球(自定义)' }
+  { id: 1, name: '足球(6人制)' },
+  { id: 2, name: '足球(8人制)' },
+  { id: 3, name: '足球(11人制)' },
+  { id: 4, name: '足球(自定义)' }
 ];
 
 export const TEAM_TYPES = [
-  { id: 0, name: '足球' },
-  { id: 1, name: '其他' }
+  { id: 1, name: '足球' },
+  { id: 2, name: '其他' }
 ];
 
 export const SIGNUP_TYPES = {
-  attend: { id: 0, txt: '报名' },
-  pending: { id: 1, txt: '待定' },
-  absent: { id: 2, txt: '请假' }
+  attend: { id: 1, txt: '报名' },
+  pending: { id: 2, txt: '待定' },
+  absent: { id: 3, txt: '请假' }
 };
